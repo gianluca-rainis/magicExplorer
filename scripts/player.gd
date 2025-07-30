a18104719af1242264280b1447410b8673914e6a
gianluca-rainis <gianlucarainis@gmail.com>
2025-07-30 17:31:27 +0200

    Added the tilemap (not working).

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,20 +6,29 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	'''if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY'''
-
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var input_vector = Vector2.ZERO
+	
+	input_vector.x = Input.get_axis("ui_left", "ui_right")
+	input_vector.y = Input.get_axis("ui_up", "ui_down")
+	
+	if input_vector != Vector2.ZERO:
+		input_vector = input_vector.normalized()
+		
+	if input_vector.x != 0:
+		if input_vector.x < 0:
+			$AnimatedSprite2D.play("runSide")
+		else:
+			$AnimatedSprite2D.play("runSide")
+	elif input_vector.y != 0:
+		if input_vector.y < 0:
+			$AnimatedSprite2D.play("runBack")
+		else:
+			$AnimatedSprite2D.play("runFrontal")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.play("default")
+	
+	
+	velocity = input_vector * SPEED
 
 	move_and_slide()
